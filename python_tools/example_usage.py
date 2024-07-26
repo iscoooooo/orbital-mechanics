f282620941ac09c9b7cd2d72261a68c143a39277
Francisco Sanudo <francisco.sanudo10@gmail.com>
2024-07-25 21:39:19 -0700

    Modified example_usage.py to test plot3( ) method

--- python_tools/example_usage.py
+++ python_tools/example_usage.py
@@ -1,10 +1,9 @@
 from Spacecraft     import Spacecraft as SC
 from planetary_data import earth
-import math              as m
-import matplotlib.pyplot as plt
+import math             as m
 
 # Initial Conditions
-sma   = earth[ 'radius' ] + 758.63 # Semi-major axis
+sma   = earth[ 'radius' ] + 5000 # Semi-major axis
 e     = 0                          # Eccentricity
 i     = 98.43*(m.pi/180)           # Inclination
 raan  = 0                          # Right Ascension of Ascending Node
@@ -17,20 +16,11 @@ coes = [ sma, e, i, raan, omega, ta ]
 # Create an instance of the 'Spacecraft' class
 sc = SC({
     'cb'    : earth,
-    'coes'  : [ earth[ 'radius' ] + 758.63, 0, 98.43*(m.pi/180), 0, 0, 0],
+    'coes'  : coes,
     'tspan' : '1',
     'atol'  : 1e-9,
     'rtol'  : 1e-9
 })
 
-# Plot orbit
-ax = plt.figure().add_subplot(projection='3d')
-ax.plot(
-    sc.states[ :, 0 ], 
-    sc.states[ :, 1 ], 
-    sc.states[ :, 2 ], 
-    label = 'orbit'
-)
-ax.legend()
-
-plt.show()
+# Show 3D plot
+sc.plot3( 'Molniya', 'b' )
