cc824ee2d77c245eebd921e7c406852a252fb6c6
Francisco Sanudo <francisco.sanudo10@gmail.com>
2024-08-02 13:04:32 -0700

    Modified rotation matrices in numerical_tools to accept a rotation angle in degrees

--- python_tools/numerical_tools.py
+++ python_tools/numerical_tools.py
@@ -6,30 +6,39 @@ Numerical tools
 import numpy as np
 
 
-def R1(angle):
+def R1(angle, deg = False):
         '''
         Returns DCM for single rotation about the first coordinate axis
         '''
+        if deg == True:
+                angle *= np.pi/180
+                
         return np.array([
                 [ 1,        0,              0       ],
                 [ 0,  np.cos(angle),  np.sin(angle) ],
                 [ 0, -np.sin(angle),  np.cos(angle) ]        
         ])
     
-def R2(angle):
+def R2(angle, deg = False):
         '''
         Returns DCM for single rotation about the second coordinate axis
         '''
+        if deg == True:
+                angle *= np.pi/180
+
         return np.array([
                 [  np.cos(angle),  0,  np.sin(angle) ],
                 [        0,        1,        0       ],
                 [ -np.sin(angle),  0,  np.cos(angle) ]
         ])
 
-def R3(angle):
+def R3(angle, deg = False):
         '''
         Returns DCM for single rotation about the third coordinate axis
         '''
+        if deg == True:
+                angle *= np.pi/180
+
         return np.array([
                 [  np.cos(angle),  np.sin(angle),  0 ],
                 [ -np.sin(angle),  np.cos(angle),  0 ],
