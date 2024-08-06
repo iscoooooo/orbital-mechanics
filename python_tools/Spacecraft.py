054dc220c43fb9c6834ebafdb55ebd29d76e50e3
Francisco Sanudo <francisco.sanudo10@gmail.com>
2024-08-06 10:21:04 -0700

    Added method for adding perturbing accelerations during the propagation, also added a J2 pert method

--- python_tools/Spacecraft.py
+++ python_tools/Spacecraft.py
@@ -17,14 +17,15 @@ import plotting_tools   as pt
 
 def null_config():
     return {
-        'cb'         : pd.earth,
-        'coes'       : [],
-        'state'      : [],
-        'tspan'      : '1',
-        'propagator' : 'RK45',
-        'atol'       : 1e-6,
-        'rtol'       : 1e-6,
-        'propagate'  : True
+        'cb'          : pd.earth,
+        'coes'        : [],
+        'state'       : [],
+        'tspan'       : '1',
+        'propagator'  : 'RK45',
+        'atol'        : 1e-6,
+        'rtol'        : 1e-6,
+        'propagate'   : True,
+        'orbit_perts' : {}
     }
 
 REFERENCE_TIME = '2000-01-01T07:00:00'
@@ -51,15 +52,29 @@ class Spacecraft:
             self.config[ 'tspan' ] = float( self.config[ 'tspan' ] ) * \
                     oc.period_from_sv( self.config[ 'state' ], self.cb[ 'mu' ] )
 
+        # Perturbation assignment
+        self.orbit_perts = self.config[ 'orbit_perts' ]
+
+        # Set initial status for calculations
+        self.latlons_calculated = False
+
+        # Set initial states
+        self.state0       = np.zeros( 6 )
+        self.state0[ :6 ] = self.config[ 'state' ]
+
+        # Assign orbit perturbation functions to a 
+        self.assign_orbit_perturbations_functions()
+
+        # Propagates the orbit
         if self.config[ 'propagate' ]:
             self.propagate_orbit()
-
-        self.latlons_calculated = False
 
     def diffy_q( self, t, states):
 
         # Unpack states
         x, y, z, vx, vy, vz = states
+
+        states_dot = np.zeros( 6 )
 
         # Position vector and its norm
         r     = np.array( [ x, y, z ] ) 
@@ -71,11 +86,31 @@ class Spacecraft:
         # Acceleration vector
         a = - self.cb[ 'mu' ] * r / rnorm**3
 
+        # Include perturbations, if any
+        for pert in self.orbit_perts_funcs:
+            a += pert( states )
+
         # State vector rates
-        states_dot = np.concatenate( (v, a) )
+        states_dot[ :3 ]  = v
+        states_dot[ 3:6 ] = a
 
         return states_dot
     
+    def assign_orbit_perturbations_functions( self ):
+
+        self.orbit_perts_funcs_map = {
+            'J2'    : self.calc_J2,
+            'SRP'   : self.calc_SRP,
+            'ATM'   : self.calc_atm_drag,
+            '3body' : self.calc_third_body_perts
+        }
+
+        self.orbit_perts_funcs = []
+
+        for key in self.config[ 'orbit_perts' ]:
+            self.orbit_perts_funcs.append(
+                self.orbit_perts_funcs_map[ key ]
+            )
 
     def propagate_orbit( self ):
 
@@ -97,6 +132,31 @@ class Spacecraft:
     def calc_latlons( self ):
         self.latlons = oc.cart2lat( self.states[ :, :3], self.times, REFERENCE_TIME )
         self.latlons_calculated = True
+    
+    def calc_J2( self, state ):
+        '''
+        Returns the perturbing gravitational acceleration vector (p) due to J2
+        '''
+        x, y, z = state[ :3 ] 
+        r       = np.linalg.norm( state[ :3 ] )
+        p       = np.zeros( 3 )
+
+        tx = x / r * ( 5 * z**2 / r**2 - 1 )
+        ty = y / r * ( 5 * z**2 / r**2 - 1 )
+        tz = z / r * ( 5 * z**2 / r**2 - 3 )
+
+        p = 3 / 2 * self.cb[ 'J2' ] * self.cb[ 'mu' ] * self.cb[ 'radius' ]**2 / r**4 * np.array( [ tx, ty, tz ] )
+
+        return p
+
+    def calc_SRP( self, state ):
+        pass
+
+    def calc_atm_drag( self, state ):
+        pass
+
+    def calc_third_body_perts( self ):
+        pass
 
     def plot3( self, label_name, color ):
         pt.plot_3d( self.states[ :, :3], self.cb[ 'radius' ], plt_label = label_name, traj_color = color )
